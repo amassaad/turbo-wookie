a5e990a1ff8a21c93f2e9e618fd569b8b9613e79
Alex Massaad <alex.massaad@jadedpixel.com>
2014-06-10 14:56:16 -0400

    Updating file to have error methods

--- error.rb
+++ error.rb
@@ -15,3 +15,11 @@ Of a bicycle made for two. "
   end
   twiml.text
 end
+
+get '/smserror' do
+  twimlsms = Twilio::TwiML::Response.new do |r|
+    r.Message "Something seems to be down with the magic computers. Is it the Y2k bug? Daisy, Daisy,
+Give me your answer do"
+  end
+  twimlsms.text
+end
