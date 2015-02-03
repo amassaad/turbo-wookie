621c71f1774a38839b54a3f042a79a47b1e75b01
amassaad <alex.massaad@shopify.com>
2015-02-03 17:06:29 -0500

    Newrelics

--- error.rb
+++ error.rb
@@ -1,7 +1,8 @@
 require 'rubygems'
 require 'twilio-ruby'
 require 'sinatra'
- 
+require 'newrelic_rpm'
+
 get '/error' do
   twiml = Twilio::TwiML::Response.new do |r|
     r.Say "Something seems to be down with the magic computers. Is it the Y2k bug? Daisy, Daisy,
